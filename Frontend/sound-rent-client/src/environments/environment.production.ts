1908816bcd2b4e7e692ae832d00a0c0825155d5b
chanileibovich <c0548559199@gmail.com>
2026-05-15 00:20:52 +0300

    Fix API base URL to point to Google Cloud

--- Frontend/sound-rent-client/src/environments/environment.production.ts
+++ Frontend/sound-rent-client/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: '/api'
+  apiBaseUrl: 'https://nyschedule-393099053805.europe-west1.run.app/api'
 };
--- Frontend/sound-rent-client/src/environments/environment.ts
+++ Frontend/sound-rent-client/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7133/api'
+  apiBaseUrl: 'http://localhost:5037/api'
 };
